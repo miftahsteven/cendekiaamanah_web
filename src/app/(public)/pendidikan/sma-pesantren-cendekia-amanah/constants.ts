00cff71b28edf61046f36a0a8424a5548d8f7408
Miftah Syarief <miftahsyarief@mscode-lab.local>
2026-08-03 07:38:39 +0700

    ubah label button

--- src/app/(public)/pendidikan/sma-pesantren-cendekia-amanah/constants.ts
+++ src/app/(public)/pendidikan/sma-pesantren-cendekia-amanah/constants.ts
@@ -37,7 +37,7 @@ export const SMA_HERO_CONTENT = {
   email: "pesantrencendekia.amanah@gmail.com",
   phone: "+62858-8866-3587",
   registrationLink: "https://bit.ly/SPMB_SMAPCA_26-27",
-  registrationLabel: "DAFTAR SPMB TA 26/27",
+  registrationLabel: "DAFTAR SPMB TA 27/28",
 };
 
 export const SMA_MOTTO_CONTENT = {
